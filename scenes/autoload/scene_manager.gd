1f94fb219545061b6f5b5d4510d48b845595967f
what-is-a-git <138817584+what-is-a-git@users.noreply.github.com>
2025-01-02 01:05:38 -0500

    slightly better transition before i go sleep

--- scenes/autoload/scene_manager.gd
+++ scenes/autoload/scene_manager.gd
@@ -7,8 +7,12 @@ var tween: Tween
 signal scene_changed()
 
 
+func _ready() -> void:
+	transition.material.set_shader_parameter('progress', 0.0)
+	visible = false
+
+
 func switch_to(path: String, use_transition: bool = true) -> void:
-	# tehe
 	if not Config.get_value('interface', 'scene_transitions'):
 		use_transition = false
 	
@@ -19,13 +23,12 @@ func switch_to(path: String, use_transition: bool = true) -> void:
 	var killed := is_instance_valid(tween) and tween.is_running()
 	if killed:
 		tween.kill()
-	
 	if use_transition:
 		tree.current_scene.process_mode = Node.PROCESS_MODE_DISABLED
 		visible = true
 		tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 		tween.tween_property(transition.material, 'shader_parameter/progress', 1.0, 0.5)
-		tween.tween_callback(func():	
+		tween.tween_callback(func():
 			if is_instance_valid(tween) and tween.is_running():
 				tween.kill()
 			
@@ -39,7 +42,7 @@ func switch_to(path: String, use_transition: bool = true) -> void:
 		)
 	else:
 		if killed:
-			transition.material.set('shader_parameter/progress', 0.0)
+			transition.material.set_shader_parameter('progress', 0.0)
 			visible = false
 		
 		tree.call_deferred('change_scene_to_file', path)
